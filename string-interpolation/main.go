61418b974a7971e120e75eda6a670f0b3c43811e
Bhagawat Adhikari <bhagawat.adhikari@selise.ch>
2023-09-21 11:55:55 +0600

    modify code

--- string-interpolation/main.go
+++ string-interpolation/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/eiannone/keyboard"
 )
 
 var reader *bufio.Reader
@@ -14,7 +17,7 @@ type User struct {
 	UserName string
 	Age      int
 	FavNum   float64
-	OwnADog  bool
+	OwnsADog bool
 }
 
 func main() {
@@ -25,15 +28,17 @@ func main() {
 	user.UserName = readString("What is your name?")
 	user.Age = readInt("How old are you?")
 	user.FavNum = readFloat("What is you Favourite Number?")
+	user.OwnsADog = readBool("Do you own a dog? (y/n)")
 
 	// fmt.Println("your name is "+userName+". You are", age, "years old")
 
 	// fmt.Printf(fmt.Sprintf("Your name is %s and you are %d years old ", userName, age))
 
-	fmt.Printf("Your name is %s and you are %d years old and your favourite is %.4f.",
+	fmt.Printf("Your name is %s and you are %d years old and your favourite is %.4f. Owns a dog: %t.",
 		user.UserName,
 		user.Age,
-		user.FavNum)
+		user.FavNum,
+		user.OwnsADog)
 }
 
 func prompt() {
@@ -100,4 +105,33 @@ func readFloat(s string) float64 {
 			return num
 		}
 	}
+}
+
+func readBool(s string) bool {
+	err := keyboard.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	for {
+		fmt.Println(s)
+		char, _, err := keyboard.GetSingleKey()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if strings.ToLower(string(char)) != "y" && strings.ToLower(string(char)) != "n" {
+			fmt.Println("Please type y or n")
+		} else if char == 'n' || char == 'N' {
+			return false
+		} else if char == 'y' || char == 'Y' {
+			return true
+		}
+	}
+
 }
